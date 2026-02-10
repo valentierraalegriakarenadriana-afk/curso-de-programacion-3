dea35fd4aef7f5ac2132fe7d9f7f2c9c4d1f2697
valentierraalegriakarenadriana-afk <valentierraalegriakarenadriana@gmail.com>
2026-02-10 17:08:16 +0000

    ejercicio de programacion

--- semana2/Ejercicio1.java
+++ semana2/Ejercicio1.java
@@ -9,7 +9,7 @@ public class Ejercicio1 {
         for (int i = 0; i < a.length; i++) {
             System.out.println("a[" + i + "]=" + a[i]);
         }
-        //sumar los numeros pares e impares del arregloo y mostrar el resultado
+        //sumar los numeros pares e impares del arreglo y mostrar el resultado
         int sumapares = 0, sumaImpares = 0;
         for (int i _= 0; i < a.length; i++) {
             if(a[i]%2 == 0) {
