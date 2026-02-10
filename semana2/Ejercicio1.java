2e6628e02a2cc5b1018dedc6af519fecf4278abb
valentierraalegriakarenadriana-afk <valentierraalegriakarenadriana@gmail.com>
2026-02-10 16:26:53 +0000

    primer ejercicio con arreglos

---
+++ semana2/Ejercicio1.java
@@ -0,0 +1,9 @@
+package semana2;
+
+public class Ejercicio1 {
+    public static void main (String[] args) {
+        // forma1 -> Definiendo directamente los valores del arreglo
+        int
+
+    } 
+}
