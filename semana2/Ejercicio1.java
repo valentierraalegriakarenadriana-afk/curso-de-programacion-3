23f93b6e7ac7f648373a7542fd18907b66a30d0e
valentierraalegriakarenadriana-afk <valentierraalegriakarenadriana@gmail.com>
2026-02-10 16:40:10 +0000

    mi primer programa

--- semana2/Ejercicio1.java
+++ semana2/Ejercicio1.java
@@ -1,9 +1,14 @@
 package semana2;
 
 public class Ejercicio1 {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         // forma1 -> Definiendo directamente los valores del arreglo
-        int
+        int[] a = { 4, 8, 9, 6, 1, 2, 10 };
 
-    } 
+        // iterar los elementos del arreglo
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("a[" + i + "]=" + a[i]);
+        }
+
+    }
 }
